56fac99bf7c97b95ccb198817a51a436725e601f
John Coene <jcoenep@gmail.com>
2024-03-20 22:47:34 +0100

    chore: rename

--- src/index.js
+++ src/index.js
@@ -1,26 +1,4 @@
 import { Witty } from "./class";
 import { Query, emitEvent } from "./events/utils";
 
-const w = new Witty();
-await w.run();
-
-const jsonRowContent = [
-  { col1: 1, col2: "foo" },
-  { col1: 2, col2: "bar" },
-];
-await w.db.registerFileText("rows.json", JSON.stringify(jsonRowContent));
-await w.con.insertJSONFromPath("rows.json", { name: "rows" });
-
-emitEvent({
-  id: 1,
-  query: new Query().from("rows").select("col1", "col2"),
-});
-
-emitEvent({
-  id: 2,
-  query: new Query().from("rows").select("col1", "col2").where("col1", ">", 1),
-});
-
-w.on((data) => {
-  console.info(data);
-});
+export { Witty, Query, emitEvent };
